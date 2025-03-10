a3ab6f76e0382fb5ec3e1fdc8685f805af240e10
teru <85771338+teruteru214@users.noreply.github.com>
2025-03-10 17:10:20 +0900

    Feat: 新しいDBのスキーマをgenerateし、migrateする

---
+++ src/db/schema/tag.ts
@@ -0,0 +1,45 @@
+import {
+	bigint,
+	boolean,
+	index,
+	mysqlTable,
+	serial,
+	unique,
+	varchar,
+} from "drizzle-orm/mysql-core";
+import { users } from "./user";
+import { words } from "./word";
+
+export const tags = mysqlTable(
+	"tags",
+	{
+		id: serial("id").primaryKey(),
+		userId: bigint("user_id", { mode: "number", unsigned: true })
+			.notNull()
+			.references(() => users.id, { onDelete: "cascade" }),
+		name: varchar("name", { length: 15 }).notNull(),
+		public: boolean("public").notNull().default(false),
+	},
+	(table) => ({
+		userTagUnique: unique("idx_tags_user_name").on(table.userId, table.name),
+	}),
+);
+
+export const tag_words = mysqlTable(
+	"tag_words",
+	{
+		id: serial("id").primaryKey(),
+		tagId: bigint("tag_id", { mode: "number", unsigned: true })
+			.notNull()
+			.references(() => tags.id, { onDelete: "cascade" }),
+		wordId: bigint("word_id", { mode: "number", unsigned: true })
+			.notNull()
+			.references(() => words.id, { onDelete: "cascade" }),
+		position: varchar("position", { length: 20 }).notNull(),
+	},
+	(table) => ({
+		tagWordIndex: index("idx_tag_words").on(table.tagId, table.wordId),
+		wordTagIndex: index("idx_word_tags").on(table.wordId, table.tagId),
+		tagWordUnique: unique("idx_tag_words_unique").on(table.tagId, table.wordId),
+	}),
+);
